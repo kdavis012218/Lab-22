6d8d593fea9e79aaca2b6b84a72045e57458b97f
kdavis012218 <35358468+kdavis012218@users.noreply.github.com>
2018-02-27 15:43:14 -0500

    lab 22 without animation

---
+++ lab 22/Controllers/Coffee_Shop_DBController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using lab_22.Models;
+
+namespace lab_22.Controllers
+{
+    public class Coffee_Shop_DBController : Controller
+    {
+        private Coffee_Shop_DBEntities db = new Coffee_Shop_DBEntities();
+
+        // GET: Coffee_Shop_DB
+        public ActionResult Index()
+        {
+            return View(db.Coffee_Shop_DBs.ToList());
+        }
+
+        // GET: Coffee_Shop_DB/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Coffee_Shop_DB coffee_Shop_DB = db.Coffee_Shop_DBs.Find(id);
+            if (coffee_Shop_DB == null)
+            {
+                return HttpNotFound();
+            }
+            return View(coffee_Shop_DB);
+        }
+
+        // GET: Coffee_Shop_DB/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Coffee_Shop_DB/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,Description,Quantity,Price,Items")] Coffee_Shop_DB coffee_Shop_DB)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Coffee_Shop_DBs.Add(coffee_Shop_DB);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(coffee_Shop_DB);
+        }
+
+        // GET: Coffee_Shop_DB/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Coffee_Shop_DB coffee_Shop_DB = db.Coffee_Shop_DBs.Find(id);
+            if (coffee_Shop_DB == null)
+            {
+                return HttpNotFound();
+            }
+            return View(coffee_Shop_DB);
+        }
+
+        // POST: Coffee_Shop_DB/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,Description,Quantity,Price,Items")] Coffee_Shop_DB coffee_Shop_DB)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(coffee_Shop_DB).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(coffee_Shop_DB);
+        }
+
+        // GET: Coffee_Shop_DB/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Coffee_Shop_DB coffee_Shop_DB = db.Coffee_Shop_DBs.Find(id);
+            if (coffee_Shop_DB == null)
+            {
+                return HttpNotFound();
+            }
+            return View(coffee_Shop_DB);
+        }
+
+        // POST: Coffee_Shop_DB/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Coffee_Shop_DB coffee_Shop_DB = db.Coffee_Shop_DBs.Find(id);
+            db.Coffee_Shop_DBs.Remove(coffee_Shop_DB);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ lab 22/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("lab_22")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("lab_22")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d13630f7-7a6b-4b83-aee9-93bb0a575bad")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
